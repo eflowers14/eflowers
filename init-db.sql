c3de5325fc262686b8bbb417ca75facbf074cc3c
eflowers <enriquealejandrofloresmarin@gmail.com>
2025-11-13 20:01:59 -0500

    esta va siendo mi mejor version, falta add muchos mas componentes

---
+++ init-db.sql
@@ -0,0 +1,10 @@
+-- Crear base de datos 
+CREATE DATABASE skins_db;
+
+-- Conectar a las base de datos 
+\c skins_db
+
+-- Crear tablas como ya vimos 
+CREATE TABLE users (...);
+CREATE TABLE skins (...);
+-- etc
